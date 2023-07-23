81d6a98d1f50e73cd0b4b55560cd65c441d363e4
McLytar <gianluca.brian.94@gmail.com>
2023-07-23 19:27:57 +0200

    Fix service not notifying systemd when ready

--- tusk-server/src/os/unix.rs
+++ tusk-server/src/os/unix.rs
@@ -1,7 +1,11 @@
+use systemd::daemon;
 use systemd_journal_logger::JournalLog;
 
 pub fn run() -> std::io::Result<()> {
     let server = crate::server_spawn()?;
+
+    daemon::notify(false, [(daemon::STATE_READY, 1)])?;
+
     crate::server_run(server)?;
     Ok(())
 }
